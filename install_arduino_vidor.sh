bfaea863b6a46eb300185b82d5cece9a0f8c2048
Erik Hemming <eriquesnk@gmail.com>
2019-06-24 13:48:17 +0200

    use curl instead of wget

--- install_arduino_vidor.sh
+++ install_arduino_vidor.sh
@@ -34,15 +34,16 @@ fi
 
 # if not already cached, download and install arduino IDE
 if [ ! -f "$ARDUINO_PATH/arduino" ]; then
-  echo -ne "${ORANGE}DOWNLOADING... "
-  wget https://downloads.arduino.cc/arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz
+  echo -ne "${ORANGE}DOWNLOADING... \n${LCYAN}"
+  curl -f -# https://downloads.arduino.cc/arduino-${ARDUINO_IDE_VERSION}-linux64.tar.xz -o arduino.tar.xz
+  echo -ne "${ORANGE}                    DOWNLOADED "
   if [ $? -ne 0 ]; then echo -ne "${FAIL_MSG} "; else echo -ne "${PASS_MSG} "; fi
   echo -ne "${ORANGE}UNPACKING... "
   [ ! -d "$ARDUINO_PATH/" ] && mkdir "$ARDUINO_PATH"
-  tar xf arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz -C "$ARDUINO_PATH/" --strip-components=1
+  tar xf arduino.tar.xz -C "$ARDUINO_PATH/" --strip-components=1
   if [ $? -ne 0 ]; then echo -e "${FAIL_MSG}"; else echo -e "${PASS_MSG}"; fi
   touch "$ARDUINO_PATH/$ARDUINO_IDE_VERSION"
-  rm arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz*
+  if [ -f arduino.tar.xz ]; then rm arduino.tar.xz; fi
 else
   echo -e "${ORANGE}CACHED ${PASS_MSG}"
 fi
